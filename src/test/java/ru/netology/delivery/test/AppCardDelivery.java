4e224c47adcff267e32c8d9df6d5d222bff71b06
BZza <bzza_yar87@mail.ru>
2025-08-20 19:33:13 +0300

    AppCardDelivery corrected

--- src/test/java/ru/netology/delivery/test/AppCardDelivery.java
+++ src/test/java/ru/netology/delivery/test/AppCardDelivery.java
@@ -37,20 +37,31 @@ public class AppCardDelivery {
         $("[data-test-id='agreement']").click();
         $(Selectors.byText("Запланировать")).click();
         $(Selectors.withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
-        $("[data-test-id='success-notification'].notification__content")
-                .shouldHave(exactText("Встреча успешно запланирована на " + date1))
-                .shouldBe(visible);
+        $(Selectors.withText("Встреча успешно запланирована на " + date1));
+//        ).shouldBe
+//                (visible, Duration.ofSeconds(15));
+
+//        $("[data-test-id='success-notification'].notification__content")
+//                .shouldHave(exactText("Встреча успешно запланирована на " + date1))
+//                .shouldBe(visible);
+
         $("[data-test-id='date'] input.input__control").sendKeys(
                 Keys.chord(Keys.SHIFT, Keys.HOME) + Keys.DELETE);
         $("[data-test-id='date'] input.input__control").setValue(date2);
         $$("button").find(exactText("Запланировать")).click();
-        $("[data-test-id='success-notification'].notification__content")
-                .shouldHave(text(("У вас уже запланирована встреча на другую дату. Перепланировать?")))
-                .shouldBe(visible);
+        $(Selectors.withText("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+
+
+//        $("[data-test-id='success-notification'].notification__content")
+//                .shouldHave(text(("У вас уже запланирована встреча на другую дату. Перепланировать?")))
+//                .shouldBe(visible);
+
         $("[data-test-id='replan-notification'] button").click();
-        $("[data-test-id='success-notification'].notification__content")
-                .shouldHave(exactText("Встреча успешно запланирована на " + date2))
-                .shouldBe(visible);
+
+
+//        $("[data-test-id='success-notification'].notification__content")
+//                .shouldHave(exactText("Встреча успешно запланирована на " + date2))
+//                .shouldBe(visible);
 
 
     }
